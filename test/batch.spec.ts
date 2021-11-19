2658545dee89a767eb71098e5c37e92df59de815
Otavio Lemos <otaviolemos@gmail.com>
2021-11-19 13:28:20 -0300

    test: determine wether line can be allocated to batch

--- test/batch.spec.ts
+++ test/batch.spec.ts
@@ -8,4 +8,34 @@ describe('Batch', () => {
     batch.allocate(line)
     expect(batch.availableQuantity).toEqual(18)
   })
+
+  it('should be able to allocate if available greater than required', () => {
+    const { batch, line } = makeBatchAndLine('ELEGANT_LAMP', 20, 2)
+    expect(batch.canAllocate(line)).toBeTruthy()
+  })
+
+  it('should not be able to allocate if available smaller than required', () => {
+    const { batch, line } = makeBatchAndLine('ELEGANT_LAMP', 2, 20)
+    expect(batch.canAllocate(line)).toBeFalsy()
+  })
+
+  it('should be able to allocate if available equal to required', () => {
+    const { batch, line } = makeBatchAndLine('ELEGANT_LAMP', 2, 2)
+    expect(batch.canAllocate(line)).toBeTruthy()
+  })
+
+  it('should not be able to allocate if skus do not match', () => {
+    const batch = new Batch('batch-001', 'UNCOMFORTABLE-CHAIR', 100, undefined)
+    const lineWithDifferentSku = new OrderLine('order-123', 'EXPENSIVE-TOASTER', 10)
+    expect(batch.canAllocate(lineWithDifferentSku)).toBeFalsy()
+  })
 })
+
+interface BatchAndLine { batch: Batch, line: OrderLine }
+
+function makeBatchAndLine (sku: string, batchQuantity: number, lineQuantity: number): BatchAndLine {
+  return {
+    batch: new Batch('batch-001', sku, batchQuantity, new Date()),
+    line: new OrderLine('order-123', sku, lineQuantity)
+  }
+}

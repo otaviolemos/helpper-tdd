c9e684f92dffbd0518654eab616a412a4c48446b
Otavio Lemos <otaviolemos@gmail.com>
2021-11-22 14:03:28 -0300

    feat: order line equality

--- src/entities/order-line.ts
+++ src/entities/order-line.ts
@@ -2,4 +2,10 @@ export class OrderLine {
   constructor (readonly orderId: string,
     readonly sku: string,
     readonly quantity: number) {}
+
+  equals (other: OrderLine): boolean {
+    return this.orderId === other.orderId &&
+      this.sku === other.sku &&
+      this.quantity === other.quantity
+  }
 }

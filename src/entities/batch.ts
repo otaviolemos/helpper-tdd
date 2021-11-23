062d2c6f6992cefe1fef25a63a227c89c31ad4b0
Otavio Lemos <otaviolemos@gmail.com>
2021-11-23 14:32:19 -0300

    feat: deallocate and refactor

--- src/entities/batch.ts
+++ src/entities/batch.ts
@@ -1,20 +1,40 @@
 import { OrderLine } from './order-line'
 
 export class Batch {
+  public allocations = new Array<OrderLine>()
+
   constructor (
     public reference: string,
     public sku: string,
     public quantity: number,
     public eta?: Date,
-    public availableQuantity: number = quantity
+    public purchasedQuantity = quantity
   ) {}
 
+  get availableQuantity (): number {
+    return this.purchasedQuantity - this.allocatedQuantity()
+  }
+
+  allocatedQuantity (): number {
+    return this.allocations.reduce((partialSum, l: OrderLine) =>
+      partialSum + l.quantity, 0)
+  }
+
   allocate (line: OrderLine): void {
-    this.availableQuantity -= line.quantity
+    if (this.canAllocate(line)) {
+      this.allocations.push(line)
+    }
   }
 
   canAllocate (line: OrderLine): boolean {
     return this.availableQuantity >= line.quantity &&
       this.sku === line.sku
+  }
+
+  deallocate (line: OrderLine): void {
+    if (this.allocations.includes(line)) {
+      const index = this.allocations.indexOf(line)
+      this.allocations.splice(index)
+    }
   }
 }

92192162c2db4dc55bbf89fafbd69f938987326c
Otavio Lemos <otaviolemos@gmail.com>
2021-11-26 13:42:12 -0300

    refactor: make order lines immutable

--- src/entities/order-line.ts
+++ src/entities/order-line.ts
@@ -1,7 +1,9 @@
 export class OrderLine {
   constructor (readonly orderId: string,
     readonly sku: string,
-    readonly quantity: number) {}
+    readonly quantity: number) {
+    Object.freeze(this)
+  }
 
   equals (other: OrderLine): boolean {
     return this.orderId === other.orderId &&
